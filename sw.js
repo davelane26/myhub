49a852889ef5fe507d2c79e673573855ed4df365
davelane26 <davelane_26@yahoo.com>
2026-05-09 22:28:17 -0600

    Fix PWA paths for GitHub Pages subdirectory

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'myhub-v1';
-const ASSETS = ['/', '/index.html'];
+const ASSETS = ['/myhub/', '/myhub/index.html'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
